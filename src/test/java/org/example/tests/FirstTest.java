785ef1e15582dd9095b2c7c5c55a5ea7efe7268a
GeorgeKalyaev <g.kalyaev@yandex.ru>
2026-08-26 21:29:30 +0300

    Авторизация

--- src/test/java/org/example/tests/FirstTest.java
+++ src/test/java/org/example/tests/FirstTest.java
@@ -15,32 +15,47 @@ public class FirstTest {
     private WebDriver driver;
 
     @BeforeEach
-    void setUp() {
+    void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
     @AfterEach
-    void tearDown() {
-        if (driver != null) {
+    void tearDown(){
+        if(driver != null){
             driver.quit();
         }
     }
 
     @Test
-    void first() {
+    void first() throws InterruptedException {
         driver.get("https://www.saucedemo.com");
-
         String title = driver.getTitle();
         System.out.println("title " + title);
+//        assertTrue(title.contains("Swag Labs1"), "неверный title");
         assertEquals("Swag Labs", title, "неверный title");
+
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
 
-        String productsTitle = driver.findElement(By.className("title")).getText();
-        assertTrue(productsTitle.contains("Products"), "не залогинились — нет слова Products");
+        String productTitle = driver.findElement(By.className("title")).getText();
+        System.out.println("productTitle " + productTitle);
+        assertEquals("Products", productTitle, "неверный productTitle");
+//        assertEquals(productTitle.contains("Products"), "неверный title");
+
+
+//        driver.findElement(By.id("logout_sidebar_link")).click();
+//        assertEquals("Swag Labs", title, "неверный title");
+
+
     }
 }
+
+// подключить гит и запушить
+// На сайте https://saucedemo.com находит Username по ID и вводим туда стандарт юзер и также находим passwoed
+// нажимаем кнопку логин и проверяем
+// проверяем что залогинились и проверяем что есть  слово Products
+// загрузить в гит если все норм работает
